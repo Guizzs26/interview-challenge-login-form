a5893966e69bf3416578c7da4733c4a1002d2e55
Guilherme <guiteixeira1926@gmail.com>
2024-07-11 23:21:17 -0400

    add project structure

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div className="wrapper">
+      <div className="login-form">
+        <h1>Login Form 🐞</h1>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+        <div className="errorMessage"></div>
+        <div className="row">
+          <label htmlFor={"email"}>Email</label>
+          <input id={"email"} type={"email"} autoComplete="off" />
+        </div>
+        <div className="row">
+          <label htmlFor={"password"}>Password</label>
+          <input id={"password"} type={"password"} />
+        </div>
+
+        <div className="button">
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/login.ts
@@ -0,0 +1,18 @@
+type LoginParams = {
+  email: string;
+  password: string;
+};
+
+export function login({ email, password }: LoginParams): Promise<void> {
+  const delay = (0.7 + Math.random() * 2) * 1000;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (password === "password123" && !!email) {
+        resolve();
+      } else {
+        reject({ message: "e-mail or password wrong." });
+      }
+    }, delay);
+  });
+}
